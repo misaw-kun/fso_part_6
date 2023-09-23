de613ba24ee7c061fbfd1b5256d40764d7cba593
wasim <w.akram.1912@protonmail.com>
2023-09-23 22:00:58 +0530

    init + 6.1-6.2 done

---
+++ 6.1-6.2/README.md
@@ -0,0 +1,5 @@
+# fso_part_6
+
+coursework for part 5 of fullstackopen 2022
+
+link :point_right: [here](https://fullstackopen.com/en/part6)
---
+++ 6.1-6.2/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+const App = () => {
+  const dispatcher = (action) => {
+    store.dispatch({
+      type: action,
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={() => dispatcher('GOOD')}>good</button>
+      <button onClick={() => dispatcher('OK')}>ok</button>
+      <button onClick={() => dispatcher('BAD')}>bad</button>
+      <button onClick={() => dispatcher('ZERO')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
